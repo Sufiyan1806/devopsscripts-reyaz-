9b1d254feb721e85967c80eee33d18cbf84b216b
Sufiyan Akhtar Shaikh <109985949+Sufiyan1806@users.noreply.github.com>
2026-01-02 17:21:42 +0530

    Update ansible.sh

--- ansible.sh
+++ ansible.sh
@@ -1,13 +1,4 @@
 amazon-linux-extras install ansible2 -y
 yum install python3 python-pip python-dlevel -y
 
-#sudo yum update -y
-#sudo yum install -y python3 python3-pip python3-devel
 
-#python3 --version
-#Python 3.x.x
-
-
-#pip3 install ansible
-#ansible --version
-#ansible [core 2.xx.x]
